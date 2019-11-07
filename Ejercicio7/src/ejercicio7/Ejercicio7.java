ae0445fb48373f231d758197c73a964cacfdace3
Fede cardozo <fedecardozo.a@gmail.com}>
2019-11-07 19:58:55 -0300

    Ejercicio7

--- Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -1,12 +1,49 @@
 package ejercicio7;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
     public static void main(String[] args) {
         
+        int m,t;
         
+        t = Inicio(); m = Inicio();
         
+        multiplo(t,m);
         
     }
+    
+    static int Inicio(){
+        
+        int x;
+        
+        System.out.println("Ingrese numero entero: ");
+        Scanner e = new Scanner(System.in);
+        x = e.nextInt();
+        
+        
+        return x;
+        
+    }
+    
+    static void multiplo(int x, int y){
+        
+        int []r = new int [x];
+        
+        System.out.println("Los multiplos de " + y + " son: " );
+        
+        for (int i = 0; i < x; i++) {
+            
+            r[i] = i * y;
+            
+            System.out.print(r[i] + " ");
+            
+        }
+        
+        System.out.println(" ");
+        
+    }
+    
     
 }
