cb7810b0ef9592e381870990e18fa05d9fe3087e
Fede cardozo <fedecardozo.a@gmail.com}>
2019-11-11 13:46:08 -0300

    Ejercicio19

---
+++ Ejercicio19/src/ejercicio19/Ejercicio19.java
@@ -0,0 +1,38 @@
+package ejercicio19;
+
+import java.util.Scanner;
+
+public class Ejercicio19 {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+    static void inicio(){
+        
+        int x;
+        
+        System.out.println("Ingrese cantidad de alumons para inscribir: ");
+        Scanner e = new Scanner(System.in);
+        x = e.nextInt();
+        
+        while(x>40){
+            
+            System.out.println("Capacidad maxima de las aulas es de 40");
+            System.out.println("Ingrese numero valido: ");
+            x = e.nextInt();
+            
+        }
+        
+    }
+    
+    static void aulas(){
+        
+        int x[][];
+        
+        
+    }
+}
